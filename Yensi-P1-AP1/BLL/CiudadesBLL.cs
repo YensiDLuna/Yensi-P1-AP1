8006f316cda8c83d26a1be1d7f8435f5d723cca6
Yensi Manuel De Luna Martinez <yensi_deluna@ucne.edu.do>
2020-10-05 18:23:03 -0400

    Add project files.

---
+++ Yensi-P1-AP1/BLL/CiudadesBLL.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yensi_P1_AP1.BLL
+{
+    public class CiudadesBLL
+    {
+    }
+}
---
+++ Yensi-P1-AP1/Entities/Ciudades.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yensi_P1_AP1.Entities
+{
+    public class Ciudades
+    {
+    }
+}
